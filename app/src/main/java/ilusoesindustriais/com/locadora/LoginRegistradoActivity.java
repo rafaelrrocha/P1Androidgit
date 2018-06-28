8f60787f2966fc6e769cf8f779c7f918b72174bb
Rafael Rocha <rajoca3000@yahoo.com.br>
2018-06-28 08:39:01 -0300

    Criando nova Activity de Login

--- app/src/main/java/ilusoesindustriais/com/locadora/LoginRegistradoActivity.java
+++ app/src/main/java/ilusoesindustriais/com/locadora/LoginRegistradoActivity.java
@@ -9,5 +9,9 @@ public class LoginRegistradoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_registrado);
+
+
+
+
     }
 }
